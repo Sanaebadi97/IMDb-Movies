a67cbdb2dd1a2e68a5da17dcf28bba6343abe5de
Sana Ebadi <sanaebadi97@gmail.com>
2019-12-22 14:31:25 +0330

    navigate from home to details fragment

--- app/src/main/java/sanaebadi/info/movieapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/sanaebadi/info/movieapp/ui/fragment/HomeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
 
 import sanaebadi.info.movieapp.R
 
@@ -13,6 +16,8 @@ import sanaebadi.info.movieapp.R
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
+
+    private var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,11 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btn.setOnClickListener{
+            navController!!.navigate(R.id.action_homeFragment_to_detailsFragment)
+        }
+    }
 }
