6cd67c06e2f9b9e2b089564854d6ff805566b214
Sana Ebadi <sanaebadi97@gmail.com>
2019-12-22 15:14:05 +0330

    Generated Movie data class

---
+++ app/src/main/java/sanaebadi/info/movieapp/model/MovieDetais.kt
@@ -0,0 +1,23 @@
+package sanaebadi.info.movieapp.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetais(
+    val budget: Int,
+    val id: Int,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val rating: Double
+)
